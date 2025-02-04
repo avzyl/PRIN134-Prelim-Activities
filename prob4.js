502bac8863f886570c529ce4f975d600f6e74d1a
Lyzza Abig <124745744+avzyl@users.noreply.github.com>
2025-02-04 12:45:58 +0800

    Update prob4.js

--- prob4.js
+++ prob4.js
@@ -2,3 +2,22 @@
 let str = "Hello World";
 // let str = numbers.toString();
 
+function toHash(arr){
+    let newStr = '';
+    let changedLetters = [];
+    
+    for (let i = 0; i < str.length ; i++ ){
+        if (i % 2 === 0){
+            newStr += '*';
+            changedLetters.push(str[i]);
+        } else {
+            newStr += (str[i]);
+        }
+    }
+
+    console.log("Input String:", str);
+    console.log("Output String:", newStr);
+    console.log("Hash Characters:", changedLetters.join(','));
+}
+
+toHash(str);
